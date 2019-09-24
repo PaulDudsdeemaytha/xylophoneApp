e84100c7e77be10e1e60da90cb38b41562772b35
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2019-09-24 11:50:37 -0700

    Incorporated AVFoundation

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -1,26 +1,26 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
-
 import UIKit
+//aka Audio Visual Foundation. This is used to import the sound functions
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
+        // This let soundURL sets up the location of where our sound is
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
         
-        
-        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
     }
     
   
